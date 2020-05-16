5d5ea37a9cbc979bec0c3df446c2596c61c5c9c9
ddxxdd-code <65443542+ddxxdd-code@users.noreply.github.com>
2020-05-16 19:43:25 +1000

    Create hello_world.c

---
+++ hello_world.c
@@ -0,0 +1,11 @@
+/* This is my first c file on github
+ * hello_world.c
+ * Created on 2020-05-16
+*/
+
+#include <stdio.h>
+
+int main(int argc, char *argv[]) {
+    printf("hello world\n");
+    return 0;
+}
